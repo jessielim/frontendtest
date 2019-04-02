73300e8b7d58847af5613b6a53ba7dead1d721fc
jessielim <twinsakura@gmail.com>
2019-04-02 20:20:57 +0800

    new front end test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,11 @@
 class PagesController < ApplicationController
+ def first_page
+ end
+
+ def login
+ end
+
+ def register
+ 	
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "pages#home"
+  get "/firstpage" , to: "pages#first_page"
+  get "/login" , to: "pages#login"
+  get "/register" , to: "pages#register"
 end
